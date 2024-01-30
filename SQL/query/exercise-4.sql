cc86a4bab8772fe80abafd70c904af424d961f5f
husen kureshi <husen.kureshi@improwised.com>
2024-01-30 12:30:20 +0530

    exercise 4 updated

--- SQL/query/exercise-4.sql
+++ SQL/query/exercise-4.sql
@@ -1,12 +1,19 @@
 -- Query-4 *** List out top 10 books by count of ratings. ***
 select
-    isbn,
-    count(isbn)
+	*
 from
-    rating_info
-group by
-    isbn
-order by
-    count(isbn) desc
-limit
-    10;
+	(
+	select
+		isbn,
+		count(book_rating),
+		rank() over(
+	order by
+		count(book_rating) desc
+                     ) as rank
+	from
+		rating_info
+	group by
+		isbn
+       )
+where
+	rank <= 10;
