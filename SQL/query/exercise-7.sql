cceeb3b13d986025d9a259dff7d83b97a533bd70
husen kureshi <husen.kureshi@improwised.com>
2024-02-01 15:31:09 +0530

    modify: exercise-7 fix queery

--- SQL/query/exercise-7.sql
+++ SQL/query/exercise-7.sql
@@ -1,12 +1,16 @@
 -- Query-7 *** List publishers with the author who has published highest number of books with that publisher. also show number of books. ***
+with author_book_with_publisher as 
+(
 select
     publisher as publisher,
     book_author as author,
-    count(isbn) as book_count
+    count(isbn) as book_count,
+    de() over (partition by publisher order by count(isbn) desc) as rank
 from
     books_details
 group by
     publisher,
     book_author
-order by
-    count(isbn) desc;
+)
+    
+select publisher, author, book_count from author_book_with_publisher where rank = 1;
