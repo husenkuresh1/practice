b56c4a5abaed4e9fdf68bbfdbe8215b6d0512b24
husen kureshi <husen.kureshi@improwised.com>
2024-02-01 13:10:03 +0530

    modify: exercise 5 correct the query as per requirement

--- SQL/query/exercise-5.sql
+++ SQL/query/exercise-5.sql
@@ -1,13 +1,25 @@
 -- Query-5 *** List out rating wise books count. (take ceiling of rating) (e.g. 10 - 10012 , 9 - 34344, 8 - 123121). ***
-select
-    avg(ceil(book_rating)) as avg_rating,
-    count(ceil(book_rating)) as count
+
+
+with avg_book_rating as (
+
+	select
+	isbn,
+	ceil(avg(book_rating)) as avg
 from
-    rating_info
-where
-    book_rating between 0
-    and 10
+	rating_info
 group by
-    book_rating
+	isbn
 order by
-    ceil(book_rating) desc;
+	avg(book_rating) desc 
+)
+select
+	avg_book_rating.avg as avg_rating,
+	count(isbn) as count
+from
+	avg_book_rating
+where avg_book_rating.avg is not null 
+group by
+	avg_book_rating.avg
+order by
+	avg_rating desc;
