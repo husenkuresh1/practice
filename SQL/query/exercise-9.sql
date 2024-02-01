d8724316d371e1d86836eb2e4a33f78dfc93f518
husen kureshi <husen.kureshi@improwised.com>
2024-02-01 13:10:44 +0530

    modify: exercise 9 change the output format

--- SQL/query/exercise-9.sql
+++ SQL/query/exercise-9.sql
@@ -1,13 +1,17 @@
 -- Query-9 *** List top 10 users who has read most books(have given ratings to more books). ***
+
 select
-    user_id as user_id,
-    count(isbn) as isbn,
-    avg(book_rating) as book_rating
+	*
 from
-    rating_info
-group by
-    user_id
-order by
-    count(*) desc
-limit
-    10;
+	(
+	select
+		user_id as user_id,
+		count(isbn) as books_count,
+		dense_rank() over(
+		order by (count(isbn)) desc) as rank
+	from
+		rating_info
+	group by
+		user_id) as sub
+where
+	sub.rank <= 10;
