6a105f82773bb2b74020905d1ef9a7e6ebaedf37
husen kureshi <husen.kureshi@improwised.com>
2024-02-06 12:32:19 +0530

    modify: apply changes as per requested

--- SQL/design/doc directory.sql
+++ SQL/design/doc directory.sql
@@ -12,32 +12,31 @@ CREATE TYPE collection_visibility_enum AS ENUM (
   'admin'
 );
 
--- for maintaining Administrators of the database
-
-CREATE TABLE IF NOT EXISTS admins (
-  admin_id SERIAL PRIMARY KEY,
-  admin_name VARCHAR NOT NULL,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+CREATE TYPE user_role_enum AS ENUM(
+  'user',
+  'admin'
 );
 
--- for maintaining all users of the system (either team admin or team member)
+-- for maintaining all users of the system (either admin or team member)
 
 CREATE TABLE IF NOT EXISTS users (
   user_id SERIAL PRIMARY KEY,
-  username VARCHAR NOT NULL,
-  user_email VARCHAR UNIQUE,
+  user_name VARCHAR(100) NOT NULL,
+  user_email VARCHAR(60) UNIQUE,
   user_password VARCHAR NOT NULL,
+  user_role user_role_enum,
   created_at TIMESTAMP,
   updated_at TIMESTAMP
 );
-CREATE INDEX IF NOT EXISTS username_index ON users (username, user_email);
+
+CREATE INDEX IF NOT EXISTS username_index ON users (user_name);
+CREATE INDEX IF NOT EXISTS user_email_index ON users USING hash (user_email);
 
 -- maintain all the permission list of the system
 
 CREATE TABLE IF NOT EXISTS permissions (
   permission_id SERIAL PRIMARY KEY,
-  permission_name VARCHAR NOT NULL,
+  permission_name VARCHAR(30) NOT NULL,
   permission_type permission_type_enum,
   created_at TIMESTAMP,
   updated_at TIMESTAMP
@@ -49,7 +48,7 @@ CREATE INDEX IF NOT EXISTS permission_index ON permissions (permission_name);
 
 CREATE TABLE IF NOT EXISTS teams (
   team_id SERIAL PRIMARY KEY,
-  team_name VARCHAR NOT NULL,
+  team_name VARCHAR(50) NOT NULL,
   team_admin INTEGER,
   is_deleted BOOLEAN DEFAULT false,
   created_at TIMESTAMP,
@@ -62,80 +61,88 @@ CREATE INDEX IF NOT EXISTS team_name_index ON teams (team_name, team_admin);
 
 CREATE TABLE IF NOT EXISTS collections (
   collection_id SERIAL PRIMARY KEY,
-  collection_name VARCHAR NOT NULL,
-  teams_team_id INTEGER,
-  users_user_id INTEGER,
+  collection_name VARCHAR(50) NOT NULL,
+  teams_id INTEGER,
+  users_id INTEGER,
   collection_visibility collection_visibility_enum,
   created_at TIMESTAMP,
-  FOREIGN KEY (teams_team_id) REFERENCES teams(team_id),
-  FOREIGN KEY (users_user_id) REFERENCES users(user_id)
+  FOREIGN KEY (teams_id) REFERENCES teams(team_id),
+  FOREIGN KEY (users_id) REFERENCES users(user_id)
 );
-CREATE INDEX IF NOT EXISTS collection_name_index ON collections (collection_name, teams_team_id);
+CREATE INDEX IF NOT EXISTS collection_name_index ON collections (collection_name);
+CREATE INDEX IF NOT EXISTS collections_owner_index ON collections (teams_id, users_id);
 
 -- all the files created by teams and their collections
 
 CREATE TABLE IF NOT EXISTS files (
   file_id SERIAL PRIMARY KEY,
-  file_name VARCHAR NOT NULL,
+  file_name VARCHAR(50) NOT NULL,
   file_url VARCHAR NOT NULL,
-  collections_collection_id INTEGER,
-  users_user_id INTEGER,
+  collections_id INTEGER,
+  users_id INTEGER,
   created_at TIMESTAMP,
   updated_at TIMESTAMP,
-  FOREIGN KEY (collections_collection_id) REFERENCES collections(collection_id),
-  FOREIGN KEY (users_user_id) REFERENCES users(user_id)
+  FOREIGN KEY (collections_id) REFERENCES collections(collection_id),
+  FOREIGN KEY (users_id) REFERENCES users(user_id)
 );
-CREATE INDEX IF NOT EXISTS file_name_index ON files (file_name, collection_id);
+CREATE INDEX IF NOT EXISTS file_name_index ON files (file_name);
+CREATE INDEX IF NOT EXISTS files_owner_index ON files (collections_id, users_id);
 
 -- records of requests for permission by users
 
-CREATE TABLE IF NOT EXISTS request_permission (
+CREATE TABLE IF NOT EXISTS requests_permission (
   request_id SERIAL PRIMARY KEY,
-  users_user_id INTEGER,
-  permissions_permission_id INTEGER,
+  users_id INTEGER,
+  permissions_id INTEGER,
   request_description TEXT,
   is_approved BOOLEAN DEFAULT false,
-  approved_by_admin_id INTEGER,
+  approved_by INTEGER,
   created_at TIMESTAMP,
   updated_at TIMESTAMP,
-  FOREIGN KEY (users_user_id) REFERENCES users(user_id),
-  FOREIGN KEY (permissions_permission_id) REFERENCES permissions(permission_id),
-  FOREIGN KEY (approved_by_admin_id) REFERENCES admins(admin_id)
+  FOREIGN KEY (users_id) REFERENCES users(user_id),
+  FOREIGN KEY (permissions_id) REFERENCES permissions(permission_id),
+  FOREIGN KEY (approved_by) REFERENCES users(user_id)
 );
-CREATE INDEX IF NOT EXISTS request_index ON request_permission (permissions_permission_id, users_user_id);
+CREATE INDEX IF NOT EXISTS request_index ON requests_permission (permissions_id, users_id);
 
 -- permissions provided to all team members
 
-CREATE TABLE IF NOT EXISTS team_permissions (
-  permissions_permission_id INTEGER,
-  teams_team_id INTEGER,
+CREATE TABLE IF NOT EXISTS teams_permissions (
+  permissions_id INTEGER,
+  teams_id INTEGER,
   created_at TIMESTAMP,
   updated_at TIMESTAMP,
-  PRIMARY KEY (permissions_permission_id, teams_team_id),
-  FOREIGN KEY (teams_team_id) REFERENCES teams(team_id),
-  FOREIGN KEY (permissions_permission_id) REFERENCES permissions(permission_id)
+PRIMARY KEY (permissions_id, teams_id),
+  FOREIGN KEY (teams_id) REFERENCES teams(team_id),
+  FOREIGN KEY (permissions_id) REFERENCES permissions(permission_id)
 );
+
+CREATE INDEX IF NOT EXISTS teams_permissions_index ON teams_permissions(permissions_id, teams_id);
 
 -- extra permissions provided to team admins
 
 CREATE TABLE IF NOT EXISTS team_admin_permissions (
-  teams_team_id INTEGER,
-  permissions_permission_id INTEGER,
+  teams_id INTEGER,
+  permissions_id INTEGER,
   created_at TIMESTAMP,
   updated_at TIMESTAMP,
-  PRIMARY KEY (teams_team_id, permissions_permission_id),
-  FOREIGN KEY (teams_team_id) REFERENCES teams(team_id),
-  FOREIGN KEY (permissions_permission_id) REFERENCES permissions(permission_id)
+  PRIMARY KEY (teams_id, permissions_id),
+  FOREIGN KEY (teams_id) REFERENCES teams(team_id),
+  FOREIGN KEY (permissions_id) REFERENCES permissions(permission_id)
 );
+
+CREATE INDEX IF NOT EXISTS team_admin_permissions_index ON team_admin_permissions(teams_id, permissions_id);
 
 -- team and their members mapping
 
-CREATE TABLE IF NOT EXISTS users_teams (
-  users_user_id INTEGER,
-  teams_team_id INTEGER,
+CREATE TABLE IF NOT EXISTS teams_users (
+  users_id INTEGER,
+  teams_id INTEGER,
   created_at TIMESTAMP,
   updated_at TIMESTAMP,
-  PRIMARY KEY (users_user_id, teams_team_id),
-  FOREIGN KEY (users_user_id) REFERENCES users(user_id),
-  FOREIGN KEY (teams_team_id) REFERENCES teams(team_id)
+  PRIMARY KEY (users_id, teams_id),
+  FOREIGN KEY (users_id) REFERENCES users(user_id),
+  FOREIGN KEY (teams_id) REFERENCES teams(team_id)
 );
+
+CREATE INDEX IF NOT EXISTS team_users_index ON teams_users(users_id, teams_id);
